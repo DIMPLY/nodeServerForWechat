56c15bdf3499d577bd1a57266dd883f75ca88611
Yan Yang <dimply@126.com>
2015-07-29 18:17:11 +0800

    change join method

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname));
 app.get('/test',function(req,res){
     //if(!req.query)return res.sendStatus(400);
     var temparr = [req.query.timestamp, req.query.nonce, 'reading_yan'];
-    var sigStr = temparr.sort().join();
+    var sigStr = temparr.sort().join('');
     console.log(sigStr);
     //if(sha1(sigStr)===req.query.signature)
     res.send(req.query.echostr);
