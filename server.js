f0b0aa63ac3a9cc88ca64ad8f5e538c9550bc6b1
Yan Yang <dimply@126.com>
2015-07-29 17:06:05 +0800

    function for test verification

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var lessMiddleware = require('less-middleware');
+var sha1 = require('sha1');
+//var bodyParser = require('body-parser');
+var app = express();
+//var request = require('request');
+
+//var jsonParser = bodyParser.json();
+//var urlencodedParser = bodyParser.urlencoded({extended:false});
+
+app.use(lessMiddleware(__dirname));
+app.use(express.static(__dirname));
+
+app.get('/test',function(req,res){
+    if(!req.query)return res.sendStatus(400);
+    var temparr = [req.query.timestamp, req.query.nonce, 'reading_yan'];
+    var sigStr = temparr.sort().join();
+    if(sha1(sigStr)===req.query.signature)res.send(req.query.echostr);
+});
+
+app.listen(80,function(){
+    console.log('Running Express on 80');
+});
